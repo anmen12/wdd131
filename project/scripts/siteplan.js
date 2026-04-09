1893ce573a99994200118241573ffb9b95339688
anmen12 <balistricexurbalistrice@gmail.com>
2026-04-09 16:11:41 -0600

    Week05 Siteplan 1.0

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -2,4 +2,4 @@ const year = document.querySelector("#currentyear");
 const today = new Date();
 year.innerHTML = `<span>${today.getFullYear()}</span>`;
 
-document.getElementById("lastModified").innerHTML = `<span style="color:white">Last Modification: ${document.lastModified}</span>`;
+document.getElementById("lastModified").innerHTML = `<span style="color:lightgray">Last Modification: ${document.lastModified}</span>`;
